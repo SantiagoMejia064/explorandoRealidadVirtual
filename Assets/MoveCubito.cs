75ccaff4191c418cc0ea05f092b9894f3a442628
SantiagoMejia064 <mejiasantiago0604@gmail.com>
2025-11-04 21:45:14 -0500

    palancas - inicio

---
+++ Assets/MoveCubito.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+using UnityEngine.XR.Content.Interaction;
+
+public class MoveCubito : MonoBehaviour
+{
+    public XRLever palanca;
+    public float upSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedArriba = upSpeed * (palanca.value ? 1 : 0);
+
+        Vector3 velocity = new Vector3(0, speedArriba, 0);
+        transform.position += velocity * Time.deltaTime;
+    }
+}
